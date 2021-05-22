d5e59304d689b613b1bcf6be9220a034bd928ff8
mharsh18 <68986496+mharsh18@users.noreply.github.com>
2021-05-22 17:17:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import recommendation
 app = Flask(__name__)
 CORS(app) 
         
-@app.route('/', methods=['GET'])
+@app.route('/dest', methods=['GET'])
 def recommend_destinations():
         res = recommendation.results(request.args.get('title'))
         return jsonify(res)
