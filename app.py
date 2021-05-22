41a28ae87a793df91048072ed92b8bd9749c29b5
mharsh18 <68986496+mharsh18@users.noreply.github.com>
2021-05-22 19:40:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask,request,jsonify
 from flask_cors import CORS
 import recommendation
+from json import dumps
 
 app = Flask(__name__)
 CORS(app) 
@@ -8,7 +9,7 @@ CORS(app)
 @app.route('/dest', methods=['GET'])
 def recommend_destinations():
         res = recommendation.results(request.args.get('title'))
-        return jsonify(res)
+        return dumps(res, indent=2)
 
 if __name__=='__main__':
         app.run(port = 5000, debug = True)
